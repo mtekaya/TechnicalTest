c19c1ce2d3e6c52dc1a7802795592c9359cfa69d
mtekaya <marouene.tekaya@gmail.com>
2023-04-18 15:47:59 +0200

    add some UI tests

--- TechnicalTest/TechnicalTestUITests/TechnicalTestUITests.swift
+++ TechnicalTest/TechnicalTestUITests/TechnicalTestUITests.swift
@@ -15,12 +15,68 @@ final class TechnicalTestUITests: XCTestCase {
         try super.setUpWithError()
         continueAfterFailure = false
         app = XCUIApplication()
+        app.launchArguments = ["-mock"]
         app.launch()
     }
+    
+    func testFilterCategoryExample() throws {
+        
+        let app = XCUIApplication()
+        let collectionViewsQuery = app.collectionViews
+        XCTAssert(collectionViewsQuery.cells.count == 6)
 
-    func testExample() throws {
+        let technicaltestHomeviewNavigationBar = app.navigationBars["TechnicalTest.HomeView"]
+        let filtreButton = technicaltestHomeviewNavigationBar.buttons["Filtre"]
+        filtreButton.tap()
+        let elementsQuery = app.scrollViews.otherElements
+        elementsQuery.buttons["Catégorie 3"].tap()
+        XCTAssert(collectionViewsQuery.cells.count == 0)
+        
+        filtreButton.tap()
+        elementsQuery.buttons["Catégorie 2"].tap()
+        XCTAssert(collectionViewsQuery.cells.count == 3)
+
+        filtreButton.tap()
+        elementsQuery.buttons["Catégorie 1"].tap()
+        XCTAssert(collectionViewsQuery.cells.count == 3)
+
+        filtreButton.tap()
+        elementsQuery.buttons["Toutes les catégories"].tap()
 
         
+        
+        // advertisement 6
+        collectionViewsQuery.staticTexts["test title6"].tap()
+        let siretPredicate = NSPredicate(format: "label CONTAINS[c] %@", "Siret")
+
+        XCTAssert(app.staticTexts.containing(siretPredicate).count == 0)
+        
+        
+        let backButton = app.navigationBars["TechnicalTest.AdvertisementDetailView"].buttons["Back"]
+        backButton.tap()
+        // advertisement 1
+        collectionViewsQuery.staticTexts["test title"].tap()
+
+        XCTAssert(app.staticTexts.containing(siretPredicate).count > 0)
+
+        
+        backButton.tap()
+        // advertisement 5
+        collectionViewsQuery.staticTexts["test title5"].tap()
+
+        XCTAssert(app.staticTexts.containing(siretPredicate).count == 0)
+        
+        backButton.tap()
+
+        technicaltestHomeviewNavigationBar.buttons["Tri"].tap()
+        
+        elementsQuery.buttons["Du plus ancien"].tap()
+        // ad2 should be the first cell
+        let ads2Predicate = NSPredicate(format: "label CONTAINS[c] %@", "test title2")
+        let firstCellStaticTexts = app.collectionViews.children(matching: .cell)
+            .element(boundBy: 0)
+            .staticTexts
+        XCTAssert(firstCellStaticTexts.containing(ads2Predicate).count == 1)
         
     }
 }
--- TechnicalTest/TechnicalTestUITests/TechnicalTestUITestsLaunchTests.swift
+++ TechnicalTest/TechnicalTestUITests/TechnicalTestUITestsLaunchTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 
+/*
 final class TechnicalTestUITestsLaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
@@ -30,3 +31,4 @@ final class TechnicalTestUITestsLaunchTests: XCTestCase {
         add(attachment)
     }
 }
+*/
